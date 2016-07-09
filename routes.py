02bbd335dd42efc67b38138a12c61d0ee2f33766
sandspider2234 <bnehmad@gmail.com>
2016-07-09 23:07:14 +0300

    Added basic functionality of logic tester.
    Also updated CSS.

--- routes.py
+++ routes.py
@@ -1,19 +1,21 @@
 import random
 from BinTree.BinTree import build_tree, solve_tree
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import StatementForm
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 def generate_statement_string():
     first_argument = random.choice(['x', 'y'])
-    operator = random.choice(['>', '<', '==', '!=', '>=', '<='])
+    first_operator = random.choice(['>', '<', '==', '!=', '>=', '<='])
     second_argument = random.choice([random.randint(-100, 100), 'x', 'y'])
     gate = random.choice(['||', '&&'])
     third_argument = random.choice(['x', 'y'])
     second_operator = random.choice(['>', '<', '==', '!=', '>=', '<='])
     fourth_argument = random.choice([random.randint(-100, 100), 'x', 'y'])
-    statement_str = ''.join([first_argument, ' ', operator, ' ', str(second_argument), ' ', gate,
+    statement_str = ''.join([first_argument, ' ', first_operator, ' ', str(second_argument), ' ', gate,
                              ' ', third_argument, ' ', second_operator, ' ', str(fourth_argument)])
     return statement_str
 
@@ -26,6 +28,16 @@ def home():
     tree = build_tree(statement_str)
     statement_result = solve_tree(tree, x, y)
     return render_template('home.html', x_value=str(x), y_value=str(y), statement=statement_str, result=str(statement_result))
+
+
+@app.route('/tester', methods=['GET', 'POST'])
+def tester():
+    form = StatementForm()
+    if form.validate_on_submit():
+        flash('Given statement is {0}, solution is {1}'.format(form.statement.data,
+                                                               str(solve_tree(build_tree(form.statement.data), 0, 0))))
+        redirect('/tester')
+    return render_template('tester.html', form=form)
 
 
 @app.route('/about')
