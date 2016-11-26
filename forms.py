4a839f23523f1f2d240083c29e8dae2456fd6214
TheCoreMan <dude500@gmail.com>
2016-11-26 21:22:25 +0200

    Forgot these

--- forms.py
+++ forms.py
@@ -1,7 +1,18 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms import validators
 
 
 class StatementForm(Form):
     statement = StringField('statement', validators=[validators.DataRequired(), validators.Regexp("""[^\";']""")])
+
+
+class SignupForm(Form):
+    username = StringField('username', validators=[validators.DataRequired()])
+    password = PasswordField('password', validators=[validators.DataRequired()])
+
+
+class LoginForm(Form):
+    username = StringField('username', validators=[validators.DataRequired()])
+    password = PasswordField('password', validators=[validators.DataRequired()])
+    remember_me = BooleanField('remember_me')
