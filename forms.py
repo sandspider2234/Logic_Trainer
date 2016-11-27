f2312ce7c6f2376362d1bab2c3e1b92cba4f101a
sandspider2234 <bnehmad@gmail.com>
2016-11-27 16:01:20 +0200

    Added signup form, trying to get SQL to work.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 import flask_wtf
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import validators
 
 
@@ -8,11 +8,15 @@ class StatementForm(flask_wtf.Form):
 
 
 class SignupForm(flask_wtf.Form):
+    email = StringField('email', validators=[validators.DataRequired(), validators.Email()])
     username = StringField('username', validators=[validators.DataRequired()])
-    password = PasswordField('password', validators=[validators.DataRequired()])
+    password = PasswordField('password', validators=[validators.DataRequired(), validators.EqualTo('verify', message='Passwords must match.')])
+    verify = PasswordField('verify', validators=[validators.DataRequired()])
+    register = SubmitField('Register')
 
 
 class LoginForm(flask_wtf.Form):
     username = StringField('username', validators=[validators.DataRequired()])
     password = PasswordField('password', validators=[validators.DataRequired()])
     remember_me = BooleanField('remember_me')
+    login = SubmitField('Log in')
